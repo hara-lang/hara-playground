866fec7b96c9ee175496446719ebb63e9ab78934
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-04 13:14:48 +1000

    Install responsive workspace layout controller

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { setRenderer } from "./app/context.js";
 import { importRepository, loadExamples, prepareProjectHome } from "./app/actions.js";
 import { bindEvents, setupRuntimeEvents } from "./app/events.js";
 import { render } from "./app/view.js";
+import { installWorkspaceLayout } from "./app/workspace-layout.js";
 
 setRenderer(() => render(bindEvents));
 setupRuntimeEvents();
+installWorkspaceLayout();
 
 async function start() {
   await loadExamples();
